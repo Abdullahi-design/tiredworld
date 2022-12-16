ba09d9bda32184c4e28a0af7081dfa75a3539da4
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-12-16 07:41:01 +0100

    page under construction

---
+++ pages/connectedPeople.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const connectedPeople = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2 bg-[#cfddcd]'>
+        <p className='bg-[#f3e415] p-4 rounded-full text-8xl'>😭</p>
+        <h1 className='p-5 bg-white rounded-full'>PAGE UNDER <b className='text-red-600'>HEAVY</b> CONSTRUCTION</h1>
+    </div>
+  )
+}
+
+export default connectedPeople
