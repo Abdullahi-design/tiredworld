cd2d5ffaac71cd50371a66d30bd053aace73183a
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-12-16 07:18:49 +0100

    creating google call link

--- pages/connection.tsx
+++ pages/connection.tsx
@@ -9,13 +9,17 @@ const connection = () => {
         <h1 className='text-red-500 md:text-3xl text-2xl text-center sm:pt-0 py-4'>Do you want some one to join you??</h1>
         <div className=' md:px-auto px-4 w-full'>
             <div className='md:flex md:justify-evenly block justify-center items-center w-full'>
-                <div>               
-                    <h2 className='text-green-500 text-xl'>Copy your call link/url</h2>
+                <div className=' text-center'> 
+                    <h2 className='text-green-500 text-xl pb-3'>Join a call</h2>   
+                    <a href="http://meet.google.com/new" target='_blank'>
+                        <button className='bg-green-400 text-white md:p-4 p-2 rounded-xl shadow-xl text-xl w-full'>Create call link</button>
+                    </a>           
+                    <h2 className='text-red-500 text-xl py-3'>Copy your call link/url</h2>
                     <Image src={navUrl} alt="" />
                 </div>
                 <p className='text-red-500 md:text-3xl text-2xl text-center'>OR</p>
                 <div>
-                    <p className='text-green-500 text-xl'>Copy from top url browser</p>
+                    <p className='text-red-500 text-xl'>Copy from top url browser</p>
                     <Image src={mainUrl} alt="" />
                 </div>
             </div>
@@ -25,7 +29,7 @@ const connection = () => {
                     <p className='text-red-500 text-xl'>Paste Url here</p>
                     <input type="url" name="" id="" className='border border-green-400 md:p-4 p-2 md:px-12 px-8 outline-none rounded-2xl my-2'/>
                 </div>
-                <a href="http://meet.google.com/new">
+                <a href="#">
                     <button className='bg-green-400 text-white md:p-4 p-2 rounded-xl shadow-xl text-xl'>wait for someone to join you</button>
                 </a>
             </div>
